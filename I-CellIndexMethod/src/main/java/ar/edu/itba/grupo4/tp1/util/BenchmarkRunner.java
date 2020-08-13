bf1b7ba2fd163e0c53bd5faef1742da4ded7d90c
nachito <igvidaurreta@gmail.com>
2020-08-13 08:54:22 -0300

    Update benchmark runner :construction_worker:

--- I-CellIndexMethod/src/main/java/ar/edu/itba/grupo4/tp1/util/BenchmarkRunner.java
+++ I-CellIndexMethod/src/main/java/ar/edu/itba/grupo4/tp1/util/BenchmarkRunner.java
@@ -6,6 +6,9 @@ import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -36,14 +39,23 @@ public class BenchmarkRunner {
     @OutputTimeUnit(TimeUnit.MILLISECONDS)
     @BenchmarkMode(Mode.AverageTime)
     public void cellIndexMethod(){
-        Particle p0 = new Particle(0.4, 0.3, 0, 0,"p0");
-        Particle p1 = new Particle(0.6, 0.6, 0, 1,"p1");
-        Particle p2 = new Particle(0.1, 0.4, 0, 2, "p2");
-        Particle p3 = new Particle(0.7, 0.7, 0, 3, "p3");
-        Particle p4 = new Particle(0.3, 0.1, 0, 4, "p4");
-        Particle p5 = new Particle(0.8, 0.1, 0, 5, "p5");
-        Particle p6 = new Particle(0.8, 0.3, 0, 6, "p6");
-        Particle p7 = new Particle(0.9, 0.9, 0, 7, "p7");
-        CellIndexMethod cim = new CellIndexMethod(8, (double) 1, 2, Stream.of(p0, p1, p2, p3, p4, p5, p6, p7).collect(Collectors.toList()), false, 0.4);
+        int N = 100;
+        long L = 10;
+        boolean periodic = false;
+        double rc = 0.9;
+        Integer M = 2;
+        ArrayList<Particle> particles = new ArrayList<>();
+
+        for(int i =0; i< 100; i++){
+            ThreadLocalRandom rnd = ThreadLocalRandom.current();
+            particles.add(new Particle(
+                    rnd.nextDouble(0, L),
+                    rnd.nextDouble(0, L),
+                    0.0,
+                    i,
+                    String.format("p%d", i)
+            ));
+        }
+        CellIndexMethod cim = new CellIndexMethod(N, (double) L, M, particles, periodic, rc);
     }
 }
