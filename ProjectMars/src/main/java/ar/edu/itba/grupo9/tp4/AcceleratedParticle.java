495f18a90919ed930a9c6be20bd5da929839c000
clara <cguzzetti@itba.edu.ar>
2020-10-03 12:33:51 -0300

    BeemanMethod done

--- ProjectMars/src/main/java/ar/edu/itba/grupo9/tp4/AcceleratedParticle.java
+++ ProjectMars/src/main/java/ar/edu/itba/grupo9/tp4/AcceleratedParticle.java
@@ -28,6 +28,12 @@ public class AcceleratedParticle {
         this(id, position.getX(), position.getY(), radius, mass);
     }
 
+    public AcceleratedParticle(int id, double radius, double mass){
+        this.id = id;
+        this.radius = radius;
+        this.mass = mass;
+    }
+
     public int getId() {
         return id;
     }
@@ -40,12 +46,28 @@ public class AcceleratedParticle {
         this.position = position;
     }
 
+    public void setPositionX(double x) {
+        this.setPosition(new Point2D.Double(x, this.position.getY()));
+    }
+
+    public void setPositionY(double y) {
+        this.setPosition(new Point2D.Double(this.position.getX(), y));
+    }
+
     public Point2D getVelocity() {
         return velocity;
     }
 
     public void setVelocity(Point2D velocity) {
         this.velocity = velocity;
+    }
+
+    public void setVelocityX(double vx) {
+        this.setVelocity(new Point2D.Double(vx, this.getVelocity().getY()));
+    }
+
+    public void setVelocityY(double vy) {
+        this.setVelocity(new Point2D.Double(this.getVelocity().getX(), vy));
     }
 
     public double getRadius() {
@@ -62,5 +84,13 @@ public class AcceleratedParticle {
 
     public void setPrevVelocity(Point2D prevVelocity) {
         this.prevVelocity = prevVelocity;
+    }
+
+    public Point2D getPrevPosition() {
+        return prevPosition;
+    }
+
+    public Point2D getPrevVelocity() {
+        return prevVelocity;
     }
 }
--- ProjectMars/src/main/java/ar/edu/itba/grupo9/tp4/BeemanMethod.java
+++ ProjectMars/src/main/java/ar/edu/itba/grupo9/tp4/BeemanMethod.java
@@ -29,10 +29,28 @@ public class BeemanMethod implements IntegralMethod{
 
     public void updatePosition(AcceleratedParticle nextParticle, AcceleratedParticle particle, double time) {
 
+        AcceleratedParticle prevP = new AcceleratedParticle(particle.getId(), particle.getPrevPosition(),particle.getRadius(), particle.getMass());
+
+        nextParticle.setPositionX(particle.getPosition().getX() + particle.getVelocity().getX() * deltaT
+                + (2.0/3) * (force.getForceX(time, particle)/nextParticle.getMass()) * deltaT * deltaT
+                - (1.0/6) * (force.getForceX(time - deltaT, prevP)/nextParticle.getMass()) * deltaT * deltaT);
+        nextParticle.setPositionY(particle.getPosition().getY() + particle.getVelocity().getY() * deltaT
+                + (2.0/3) * (force.getForceY(time, particle)/nextParticle.getMass()) * deltaT * deltaT
+                - (1.0/6) * (force.getForceY(time - deltaT, prevP)/nextParticle.getMass()) * deltaT * deltaT);
     }
 
     public void updateVelocity(AcceleratedParticle nextParticle, AcceleratedParticle particle, double time) {
 
+        AcceleratedParticle prevP = new AcceleratedParticle(particle.getId(), particle.getPrevPosition(),particle.getRadius(), particle.getMass());
+
+        nextParticle.setVelocityX(particle.getVelocity().getX()
+                + (1.0/3) * (force.getForceX(time, nextParticle)/nextParticle.getMass()) * deltaT
+                + (5.0/6) * (force.getForceX(time, particle)/nextParticle.getMass()) * deltaT
+                - (1.0/6) * (force.getForceX(time - deltaT, prevP)/nextParticle.getMass()) * deltaT);
+        nextParticle.setVelocityY(particle.getVelocity().getY()
+                + (1.0/3) * (force.getForceY(time, nextParticle)/nextParticle.getMass()) * deltaT
+                + (5.0/6) * (force.getForceY(time, particle)/nextParticle.getMass()) * deltaT
+                - (1.0/6) * (force.getForceY(time - deltaT, prevP)/nextParticle.getMass()) * deltaT);
     }
 
 
