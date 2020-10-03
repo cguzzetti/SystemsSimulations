f180fe0dcb7bcd0c06ff854c2a607084969c88f1
nachito <igvidaurreta@gmail.com>
2020-10-03 10:25:17 -0300

    add a description to the integral methods

--- ProjectMars/src/main/java/ar/edu/itba/grupo9/tp4/IntegralMethods.java
+++ ProjectMars/src/main/java/ar/edu/itba/grupo9/tp4/IntegralMethods.java
@@ -1,7 +1,20 @@
 package ar.edu.itba.grupo9.tp4;
 
+/**
+ *  Determines which Integral Method we are running
+ */
 public enum IntegralMethods {
-    BEEMAN,
-    VERLET,
-    GEAR_PREDICTOR_CONECTOR
+    BEEMAN ("Beeman Method"),
+    VERLET ("Verlet Type Method"),
+    GEAR_PREDICTOR_CORRECTOR ("Gear Predictor Corrector of order V"),
+    ;
+
+    final String description;
+    IntegralMethods(String description) {
+        this.description = description;
+    }
+
+    public String getDescription(){
+        return description;
+    }
 }
