fa2c7848d1dd13ded7b24406743653a58dd3c602
clara <cguzzetti@itba.edu.ar>
2020-10-04 12:14:11 -0300

    Graphics for position

---
+++ ProjectMars/src/main/java/ar/edu/itba/ss/g9/tp4/visualization/visualizer.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    fig, ax = plt.subplots()
+    ax.set_xlabel('t [s]')
+    ax.set_ylabel('Posición [m]')
+
+    methods = ["Gear predictor", "Verlet original", "Beeman", "Analítico"]
+    # Read from file
+    file = open("mars_output.xyz","r")
+    lines = file.readlines()
+
+    for i in range(0, len(methods)):
+        x = []
+        y = []
+        for line in lines[:]:
+            line = line.split()
+            print(line)
+            x.append(float(line[0]))
+            y.append(float(line[i+1]))
+
+        plt.plot(x, y, label ='Método: '+methods[i])
+
+    plt.legend(loc ="lower right")
+    plt.show()
