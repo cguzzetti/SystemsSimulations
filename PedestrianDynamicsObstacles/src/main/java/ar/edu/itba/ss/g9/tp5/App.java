421cc339ac3a5bcc6dfe2e219141249dc9051ff1
nachito <igvidaurreta@gmail.com>
2020-10-30 09:21:07 -0300

    Add new project boilerplate

---
+++ PedestrianDynamicsObstacles/src/main/java/ar/edu/itba/ss/g9/tp5/App.java
@@ -0,0 +1,13 @@
+package ar.edu.itba.ss.g9.tp5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
