7eb746c993f9ff00ac4e8b6f4a59df279ab0c992
nachito <igvidaurreta@gmail.com>
2020-10-30 13:41:50 -0300

    add argument parsing to execution script

--- PedestrianDynamicsObstacles/run.sh
+++ PedestrianDynamicsObstacles/run.sh
@@ -2,13 +2,24 @@
 set -e
 
 GREEN='\033[0;34m'
+RED='\033[0;31m'
 NORMAL='\033[0m'
+
+
 
 function check_visualization_dir() {
     if [ ! -d "$1" ]; then
       mkdir "$1"
     fi
 }
+
+MIN_ARGS=3
+
+if (( "$#" < "$MIN_ARGS" )); then
+  echo -e "${RED}Error: $# is not a valid number of arguments. You need at least $MIN_ARGS${NORMAL}"
+  echo "Usage: $0 <num_of_obstacles> deltaT deltaT2"
+  exit 1
+fi
 
 echo -e "\n\n${GREEN}Running Project Mars with arguments: $* ${NORMAL}\n\n"
 
--- PedestrianDynamicsObstacles/src/main/java/ar/edu/itba/ss/g9/tp5/Main.java
+++ PedestrianDynamicsObstacles/src/main/java/ar/edu/itba/ss/g9/tp5/Main.java
@@ -1,13 +1,22 @@
 package ar.edu.itba.ss.g9.tp5;
 
+import java.util.Arrays;
+
 /**
  * Hello world!
  *
  */
 public class Main
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        System.out.println(String.format("Running with args: %s", Arrays.toString(args)));
+        final int obstaclesNum  = Integer.parseInt(args[0]);
+        final double deltaT     = Double.parseDouble(args[1]);
+        final double deltaT2    = Double.parseDouble(args[2]);
+        System.out.println(obstaclesNum);
+        System.out.println(deltaT);
+        System.out.println(deltaT2);
+        System.out.println("Done!");
+
     }
 }
